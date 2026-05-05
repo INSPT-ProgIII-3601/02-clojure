7d87111879a1a57d10e799191b7a9a9da001aa4e
DarienPerezGit <darienperez@vamos.app>
2026-05-05 09:19:34 -0300

    docs: add README and clean exercise 2

--- ej02.clj
+++ ej02.clj
@@ -1,19 +1,21 @@
 ;;; Ejercicio 2
-;;Definir la función segundos que reciba los cuatro valores (días, horas, minutos y segundos) del tiempo que dura un evento y devuelva el valor de ese tiempo expresado solamente en segundos.
+;; Definir la funcion segundos que reciba los cuatro valores
+;; (dias, horas, minutos y segundos) del tiempo que dura un evento
+;; y devuelva ese tiempo expresado solamente en segundos.
 
-; V1 (by Elias)
-(defn segundos[a b c d](+ d (* 60 (+ c (* 60 (+ b (* 24 a)) )) )))
+(defn dias-a-segundos [dias]
+  (* dias 24 60 60))
 
-; V2 (by Fabrizio)
-(defn segundos[d h m s](+ (* d 86400)(* h 3600)(* m 60) s))
+(defn horas-a-segundos [horas]
+  (* horas 60 60))
 
-; V3 (by Ignacio)
-(defn days_to_sec [d] (* d 24 60 60)) 
+(defn minutos-a-segundos [minutos]
+  (* minutos 60))
 
-(defn hours_to_sec [h] (* h 60 60))
-
-(defn minutes_to_sec [m] (* m 60))
-
-(defn segundos [d h m s] (if (or (neg? d) (neg? h) (neg? m) (neg? s)) 
-                           (println "Alguno de los valores ingresados es invalido (negativo)") 
-                           (+ (days_to_sec d) (hours_to_sec h) (minutes_to_sec m) s)))
+(defn segundos [dias horas minutos segundos]
+  (if (or (neg? dias) (neg? horas) (neg? minutos) (neg? segundos))
+    (println "Alguno de los valores ingresados es invalido (negativo)")
+    (+ (dias-a-segundos dias)
+       (horas-a-segundos horas)
+       (minutos-a-segundos minutos)
+       segundos)))
